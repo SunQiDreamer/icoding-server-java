701dec02e0f1ef5f1d47c1b1fd545fb8dafd3cb7
sunqi <qi.sun@saxofintech.com>
2022-09-19 18:41:45 +0800

    Update

--- src/main/java/com/sq/ic/controller/BaseController.java
+++ src/main/java/com/sq/ic/controller/BaseController.java
@@ -1,4 +1,13 @@
 package com.sq.ic.controller;
 
-public class BaseController {
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.function.Function;
+
+@Validated
+public abstract class BaseController<Po,ReqVo> {
+    protected abstract IService<Po> getService();
+    protected abstract Function<ReqVo, Po> getFunction();
+
 }
